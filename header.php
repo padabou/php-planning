07bbac5c3d46457f36a06a65dbe6d629396429eb
Ayrat Yamaltdinov <septikwar@gmail.com>
2020-10-16 22:28:46 +0300

    delete extra tags in header.php

--- header.php
+++ header.php
@@ -2,13 +2,13 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="/schedule.php">Montlhy</span></a>
+                <a class="nav-link" href="/schedule.php">Montlhy</a>
             </li>
             <li class="nav-item active">
-                <a class="nav-link" href="/schedule-week.php">Weekly</span></a>
+                <a class="nav-link" href="/schedule-week.php">Weekly</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/scheduleManagement.php">Management</span></a>
+                <a class="nav-link" href="/scheduleManagement.php">Management</a>
             </li>
         </ul>
     </div>
